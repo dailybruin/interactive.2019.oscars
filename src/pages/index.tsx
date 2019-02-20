b3efae9a35b462173a2d4aff194f7cdcff7d55ef
Fe-Zhang <felix44@ucla.edu>
2019-02-19 19:29:31 -0800

    added navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import styled from 'react-emotion'
+import Star from '../images/star.png'
 import {
   Article,
   CoverPhoto,
@@ -45,8 +47,35 @@ export const query = graphql`
   }
 `
 
+const NavBar = styled('div')`
+  display: flex;
+  flex-direction: row;
+  color: #FEE9B5;
+  align-items: center;
+  justify-content: space-evenly;
+  @media (max-width: 890px) { flex-direction: column; }
+`
+
+const NavItem = styled('div')`
+  font-family: Open Sans;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  font-size: 36px;
+  margin-bottom: 0;
+`
+
+const StarDiv = () => <img src={Star} style={{marginBottom: 0}}/>
+
+
 const IndexPage = ({ data }) => (
   <>
+    <Head {...data.site.siteMetadata} />
+    <NavBar>
+      <NavItem><StarDiv/> Features <StarDiv/></NavItem>
+      <NavItem><StarDiv/> Predictions <StarDiv/></NavItem>
+      <NavItem><StarDiv/> Related Coverage <StarDiv/></NavItem>
+    </NavBar>
     <Head {...data.site.siteMetadata} />
     <CoverPhoto
       headline={data.kerckhoffArticle.headline}
